4d8119712a673b22c347f01e8c2be95410781ab0
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-07-06 16:11:11 +0530

    Add event handler

--- updater/__main__.py
+++ updater/__main__.py
@@ -3,6 +3,10 @@ import asyncio
 from watcher import Watcher
 import updater.database_api as api
 from decouple import config
+from watcher.events.quora import QuoraEvent
+import json
+import aiohttp
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(message)s",
@@ -11,7 +15,13 @@ logging.basicConfig(
 
 UPDATER_ID = config("UPDATER_ID")
 logger = logging.getLogger(__name__)
+CALLBACK_SERVER = config("CALLBACK_SERVER")
 
+
+def event_handler(event):
+    logger.info("Sending an event")
+    async with aiohttp.ClientSession() as session:
+        await session.post(CALLBACK_SERVER, json=json.dumps(event))
 
 class Server(Watcher):
     def start(self):
@@ -19,6 +29,7 @@ class Server(Watcher):
         users = api.fetch_usernames(UPDATER_ID)
         for id, usernames in users:
             self.add_quora(usernames, update_interval=3)
+        self.dispatcher.add_handler(QuoraEvent, event_handler)
         loop = asyncio.get_running_loop()
         logger.info("Creating task to run watcher")
         self.running_task = loop.create_task(self.run())
@@ -33,9 +44,9 @@ class Server(Watcher):
 
 server = Server()
 
-
 async def main():
     server.start()
+
 
 
 if __name__ == "__main__":
